a4104229d64968c503f42c73077cb59d84daf724
reuttonDEV <106014309+reutto@users.noreply.github.com>
2023-02-20 15:28:07 +0100

    Add files via upload

---
+++ Nowy folder (2)/script.js
@@ -0,0 +1,20 @@
+var objPeople  = [
+
+
+{
+username: "ok",
+password: "abcd"
+}
+{
+    username: "ok1",
+    password: "abcde"
+}
+]
+function getInfo(){
+    var username = document.getElementById('username').ariaValueMax;
+    var username = document.getElementById('password').ariaValueMax;
+
+for(var 1=0; i < objPeople.length; i++){
+    
+}
+}
